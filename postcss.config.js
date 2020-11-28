4b4ff69cea494569aa599baa005afd6646e91cf0
Matt <mattsnaccc@gmail.com>
2020-11-28 15:30:03 -0800

    prod upd

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		...(process.env.NODE_ENV === "production"
+			? {
+					"@fullhuman/postcss-purgecss": {
+						content: ["./components/**/*.js", "./pages/**/*.js"],
+						defaultExtractor: (content) =>
+							content.match(/[\w-/:]+(?<!:)/g) || [],
+					},
+			  }
+			: {}),
+	},
 };
