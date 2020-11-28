a9c32cf7eb26c00e4b3d2b8fdb349a7689f8126c
Matt <mattsnaccc@gmail.com>
2020-11-28 15:59:11 -0800

    tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-		...(process.env.NODE_ENV === "production"
-			? {
-					"@fullhuman/postcss-purgecss": {
-						content: ["./components/**/*.js", "./pages/**/*.js"],
-						defaultExtractor: (content) =>
-							content.match(/[\w-/:]+(?<!:)/g) || [],
-					},
-			  }
-			: {}),
-	},
+	plugins: ["tailwindcss", "postcss-preset-env"],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-	purge: [],
+	purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
 	presets: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
