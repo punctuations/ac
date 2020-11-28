0e262c71c51890bfbcd59f8e5f4f9d8cc4779926
Matt <mattsnaccc@gmail.com>
2020-11-28 15:39:24 -0800

    font fix?

--- pages/index.js
+++ pages/index.js
@@ -99,6 +99,11 @@ export default function Home(props) {
 			<Head>
 				<title>Animal Crossing - {hour}</title>
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.gstatic.com"></link>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+					rel="stylesheet"
+				></link>
 			</Head>
 			{backgroundArt()}
 			<div className="flex flex-row items-center justify-center z-10 absolute w-full h-full">
