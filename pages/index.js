d2eec7b662601464ba8756b0c0ac9cacdc35b6fc
punctuations <matt@dont-ping.me>
2020-11-30 10:04:38 -0800

    added github + beginning of menu

--- pages/index.js
+++ pages/index.js
@@ -183,6 +183,49 @@ export default function Home(props) {
 				>
 					{date}
 				</motion.div>
+
+				<motion.div
+					initial="initial"
+					animate="enter"
+					variants={fourth}
+					className="text-white absolute bottom-0 right-0 m-6"
+				>
+					<a href="https://github.com/punctuations/ac" target="_blank">
+						<svg
+							width="24"
+							height="24"
+							fill="currentColor"
+							className="opacity-50 hover:opacity-75 cursor-pointer"
+						>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"
+							></path>
+						</svg>
+					</a>
+				</motion.div>
+				<motion.div
+					initial="initial"
+					animate="enter"
+					variants={fourth}
+					className="text-white absolute top-0 right-0 m-6"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						className="w-7 h-7 opacity-50 hover:opacity-75 cursor-pointer"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+						/>
+					</svg>
+				</motion.div>
 				<AnimatePresence initial={false}>
 					{gameMenu && (
 						<motion.div
