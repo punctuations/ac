a984d98518054840afdb29a6922a1266fdd0d735
Matt <mattsnaccc@gmail.com>
2020-11-30 19:55:06 -0800

    mute button + todo upd

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,27 @@ export default function Home(props) {
 
 	function dragToVolume() {}
 
+	function indicator() {
+		switch (muted) {
+			case true:
+				return "/muted.svg";
+			case false:
+				return "/sound.svg";
+		}
+	}
+
+	function toggleMute() {
+		switch (muted) {
+			case false:
+				Howler.mute(true);
+				setMutedState(true);
+				break;
+			case true:
+				Howler.mute(false);
+				setMutedState(false);
+		}
+	}
+
 	function audioUp() {
 		Howler.volume(Howler.volume() + 0.1);
 	}
@@ -128,6 +149,8 @@ export default function Home(props) {
 
 	const [gameMenu, setMenu] = useState(false);
 	const [album, setAlbum] = useState(0);
+
+	const [muted, setMutedState] = useState(false);
 
 	const [playback, setPlayback] = useState(false);
 
@@ -370,18 +393,11 @@ export default function Home(props) {
 						className="font-bold p-3 text-white flex flex-row"
 						variants={third}
 					>
-						<svg // soon to be mute button
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							className="h-6 w-6 cursor-pointer"
-						>
-							<path
-								fillRule="evenodd"
-								d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
-								clipRule="evenodd"
-							/>
-						</svg>
+						<img
+							className="h-6 w-6 cursor-pointer indicator"
+							src={indicator()}
+							onClick={() => toggleMute()}
+						/>
 
 						<motion.div
 							className="shadow-sm ml-2 mt-1.5 px-1 flex flex-wrap content-center menu-bg w-32 h-3 rounded-lg"
